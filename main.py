7e6727b0edfd7ad77b580440d93d2c396d7f2f36
felipecaroca <felipe.caroca.dev@gmail.com>
2025-03-30 14:31:52 -0300

    test json

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
@@ -12,6 +12,14 @@ CORS(app)
 @app.route("/", methods=["GET"])
 def home():
   my_env = os.getenv('MY_ENV')
+  try:
+    with open('test.json', 'r', encoding='utf-8') as file:
+      data = json.load(file)  
+      data["myenv"] = my_env
+      return jsonify(data) 
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500  # Manejo de errores
+  
   return my_env if my_env else 'no-env :('
 
 if __name__ == "__main__":
